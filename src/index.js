0ccfe0139425825764120e4824723b1d90d9a67f
zahur <zahurmeerun@hotmail.com>
2021-11-10 21:08:50 +0400

    add feature to toggle background

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { animals } from './animals';
 import reportWebVitals from './reportWebVitals';
 
+
 let displayFact = (e) => {   
   const optionIndex = Math.floor(Math.random() * animals[e.target.alt].facts.length);  
   const funFact = animals[e.target.alt].facts[optionIndex]  
@@ -16,18 +17,35 @@ for (const animal in animals) {
   images.push(<img onClick={displayFact} key={animal} className='animal' alt={animal} src={animals[animal].image} aria-label={animal} role='button'/>)
 };
 
+let showBackground = true
+
+let displayBackground = (e) => {
+  console.log(showBackground)
+  if(showBackground){
+    e.target.src="images/blank.jpg";   
+    showBackground=false;
+  }else{
+    e.target.src="images/ocean.jpg";
+    showBackground=true;
+  }
+}
+
+let background = <img onClick={displayBackground} className="background" src='images/ocean.jpg'/> 
+
+
 ReactDOM.render(
  <h1 className='main-text'>react example</h1>,
   document.getElementById('root')
 );
+
 const title = ''
 const animalFacts = (
-  <div>
+  <div className='content'>
     <h1 className='main-text'>{title === '' ?  'Click an animal for a fun fact' : title}</h1>
-    {images}
+    {images} 
+    {background}  
     <p id="fact"></p> 
   </div> 
-   
 )
 
 ReactDOM.render(animalFacts, document.getElementById('main'))
